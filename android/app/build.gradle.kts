711edda83e65e9b49d4547757ac1f04016c0707d
Ales Drobysh <drobysh.ales@gmail.com>
2026-01-06 22:33:35 +0100

    Update application bundle identifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.purecal"
+    namespace = "dev.alesdrobysh.purecal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.purecal"
+        applicationId = "dev.alesdrobysh.purecal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/dev/alesdrobysh/purecal/MainActivity.kt
+++ android/app/src/main/kotlin/dev/alesdrobysh/purecal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.purecal
+package dev.alesdrobysh.purecal
 
 import io.flutter.embedding.android.FlutterActivity
 
